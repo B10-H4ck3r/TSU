0a15598370548dc7112b797a42992730332cf4e8
B10-H4ck3r <b10-h4ck3r@protonmail.com>
2021-01-13 09:43:12 -0600

    Initial Development of Website

---
+++ about.php
@@ -0,0 +1,29 @@
+<?php
+  include('head.php');
+?>
+
+<body>
+
+<?php
+  include('navigation.php');
+?>
+
+<?php
+  include('main_header.php');
+?>
+
+<!-- Image element - set the background image for the header in the line below -->
+<div class="py-5 bg-image-full" style="background-image: url('img/about-us-banner.jpeg');">
+  <!-- Put anything you want here! There is just a spacer below for demo purposes! -->
+  <div style="height: 300px;"></div>
+</div>
+
+<!-- Content section -->
+<section class="py-5">
+  <div class="container">
+    <h1 class="cnt_hdr">Our Underwriters</h1>
+    <p class="about_tsu_text">
+      Hello World
+    </p>
+  </div>
+</section>
---
+++ head.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" type="text/css" href="fonts.css">
+  <link rel="stylesheet" type="text/css" href="flex_box.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Cinzel&family=Crimson+Text&family=Lora&family=Nanum+Myeongjo&family=Redressed&display=swap" rel="stylesheet">
+  <title>Texas Specialty Underwriters</title>
+  <script src="https://kit.fontawesome.com/7bed33201a.js" crossorigin="anonymous"></script>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/full-width-pics.css" rel="stylesheet">
+
+</head>
